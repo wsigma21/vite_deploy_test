5be9ccae069902cddd38d9692d95457b0d3a0ca9
wsigma21 <wallstlbl@gmail.com>
2024-11-28 19:40:13 +0900

    vite.config.ts修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? 'REPOSITORY_NAME' : './',
+  base: '/vite_deploy_test/', 
   plugins: [react()],
 })
