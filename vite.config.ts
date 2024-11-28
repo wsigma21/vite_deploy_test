fc35e4679b9de5ece729ce1431ac8d049fd61b03
wsigma21 <wallstlbl@gmail.com>
2024-11-28 19:58:42 +0900

    元に戻す

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/deploy-test/', 
+  base: '/vite_deploy_test/', 
   plugins: [react()],
 })
