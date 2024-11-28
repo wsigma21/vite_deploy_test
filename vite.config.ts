4bfd4e30045a029b8c144c311cb4ca4f294b4bc4
wsigma21 <wallstlbl@gmail.com>
2024-11-28 17:52:52 +0900

    deploy test

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/vite_deploy_test/',
+  plugins: [react()],
+})
